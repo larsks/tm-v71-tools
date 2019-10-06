larsks/tm-v71-tools

commit 45ae387da5cc399ea88d068d282545fd2fcf79bd

Rewrite write-block/read-block to take an address
It ends up being easier to just pass in an address rather than
a block + offset.

tmv71/cli.py
@@ -135,9 +135,9 @@ def channel(ctx, band, channel):
 
 
 @main.command()
-@click.option('-c', '--control', type=int)
-@click.option('-p', '--ptt', type=int)
-@click.option('-b', '--both', '--cp', type=int)
+@click.option('-c', '--control')
+@click.option('-p', '--ptt')
+@click.option('-b', '--both', '--cp')
 @click.pass_context
 def control_band(ctx, control, ptt, both):
     '''Select control and ptt band.'''
@@ -510,11 +510,10 @@ def flexint(v):
 @click.option('-o', '--output', type=click.File('wb'),
               default=sys.stdout.buffer)
 @click.option('-h', '--hexdump', '_hexdump', is_flag=True)
-@click.argument('block')
-@click.argument('offset', type=flexint, default='0', required=False)
-@click.argument('length', type=flexint, default='0', required=False)
+@click.argument('address')
+@click.argument('size', type=flexint, default='0', required=False)
 @click.pass_context
-def read_block(ctx, output, _hexdump, block, offset, length):
+def read_block(ctx, output, _hexdump, address, size):
     '''Read one or more memory blocks from the radio.
 
     This command will by default output the binary data to stdout. Use the
@@ -527,17 +526,17 @@ def read_block(ctx, output, _hexdump, block, offset, length):
     Nothing stops you from providing an offset and size when specifying
     multiple blocks, but it probably doesn't make sense.'''
 
-    if ':' in block:
-        b_start, b_end = (flexint(x) for x in block.split(':'))
-        blocks = range(b_start, b_end + 1)
+    if ':' in address:
+        addr_start, addr_end = (flexint(x) for x in address.split(':'))
+        addr_range = range(addr_start, addr_end)
     else:
-        blocks = [flexint(block)]
+        addr_range = [flexint(address)]
 
     with output, ctx.obj.programming_mode():
-        for block in blocks:
-            LOG.info('reading %d bytes of memory from block %d offset %d',
-                     256 if length == 0 else length, block, offset)
-            data = ctx.obj.read_block(block, offset, length)
+        for addr in addr_range:
+            LOG.info('reading %d bytes of memory from %x',
+                     256 if size == 0 else size, addr)
+            data = ctx.obj.read_block(addr, size)
 
             if _hexdump:
                 output.write(
@@ -550,8 +549,7 @@ def read_block(ctx, output, _hexdump, block, offset, length):
 @memory.command()
 @click.option('-i', '--input', type=click.File('rb'))
 @click.option('-d', '--hexdata')
-@click.argument('block', type=flexint)
-@click.argument('offset', type=flexint, default='0', required=False)
+@click.argument('address', type=flexint)
 @click.argument('length', type=flexint, default='0', required=False)
 @click.pass_context
 def write_block(ctx, input, hexdata, block, offset, length):

tmv71/api.py
@@ -11,9 +11,9 @@ LOG = logging.getLogger(__name__)
 PORT_SPEED = ['9600', '19200', '38400', '57600']
 FREQUENCY_BAND = ['118', '144', '220', '300', '430', '1200']
 
-M_OFFSET_PORT_SPEED = (0x0, 0x21)
-M_OFFSET_BANDA_BAND = (0x2, 0x2)
-M_OFFSET_BANDB_BAND = (0x2, 0xE)
+M_OFFSET_PORT_SPEED = 0x21
+M_OFFSET_BANDA_BAND = 0x202
+M_OFFSET_BANDB_BAND = 0x20E
 
 
 class CommunicationError(Exception):
@@ -293,26 +293,24 @@ class TMV71:
 
     @pm
     def get_port_speed(self):
-        block, offset = M_OFFSET_PORT_SPEED
-        speed = self.read_block(block, offset, 1)
+        speed = self.read_block(M_OFFSET_PORT_SPEED, 1)
         return PORT_SPEED[int.from_bytes(speed, byteorder='big')]
 
     @pm
     def set_port_speed(self, speed):
         speed = PORT_SPEED.index(speed)
-        block, offset = M_OFFSET_PORT_SPEED
-        self.write_block(block, offset, bytes([speed]))
+        self.write_block(M_OFFSET_PORT_SPEED, bytes([speed]))
 
     @pm
     def get_frequency_band(self, band):
         if band == 0:
-            block, offset = M_OFFSET_BANDA_BAND
+            address = M_OFFSET_BANDA_BAND
         elif band == 1:
-            block, offset = M_OFFSET_BANDB_BAND
+            address = M_OFFSET_BANDB_BAND
         else:
             raise ValueError('invalid band')
 
-        res = self.read_block(2, offset, 1)
+        res = self.read_block(address, 1)
         res = int.from_bytes(res, byteorder=sys.byteorder)
 
         # The constants in FREQUENCY_BAND are correct for band A, but
@@ -330,18 +328,18 @@ class TMV71:
         freq_band += (4 * band)
 
         if band == 0:
-            block, offset = M_OFFSET_BANDA_BAND
+            address = M_OFFSET_BANDA_BAND
         elif band == 1:
-            block, offset = M_OFFSET_BANDB_BAND
+            address = M_OFFSET_BANDB_BAND
         else:
             raise ValueError('invalid band')
 
-        self.write_block(2, offset, bytes([freq_band]))
+        self.write_block(address, bytes([freq_band]))
 
     @pm
     def reset(self):
         '''Reset to default configuration'''
-        self.write_block(0, 0, b'\xff')
+        self.write_block(0, b'\xff')
 
     # ----------------------------------------------------------------------
 
@@ -353,7 +351,7 @@ class TMV71:
         context manager.  For example:
 
             with radio.programming_mode():
-                radio.read_block(0, 0, 0)
+                radio.read_block(0, 0)
 
         Failure to use the context manager will result in
         WrongModeError exception.'''
@@ -382,28 +380,32 @@ class TMV71:
                 raise UnexpectedResponseError()
 
     @pm
-    def read_block(self, block, offset, length):
+    def read_block(self, address, size):
         '''Read data from the radio'''
 
-        LOG.debug('read block %d, offset %d, length %d', block, offset, length)
-        self.write_bytes(bytes([ord('R'), block, offset, length]))
+        block = address // 256
+        offset = address-(block * 256)
+        LOG.debug('read block %d, offset %d, size %d', block, offset, size)
+        self.write_bytes(bytes([ord('R'), block, offset, size]))
         self.read_bytes(4)
-        data = self.read_bytes(length if length else 256)
+        data = self.read_bytes(size if size else 256)
         self.write_bytes(bytes([6]))
         self.check_ack()
         return data
 
     @pm
-    def write_block(self, block, offset, data):
+    def write_block(self, address, data):
         '''Write data to the radio'''
 
-        length = len(data)
-        LOG.debug('write block %d, offset %d, length %d',
-                  block, offset, length)
-        if length == 256:
-            length = 0
+        block = address // 256
+        offset = address-(block * 256)
+        size = len(data)
+        LOG.debug('write block %d, offset %d, size %d',
+                  block, offset, size)
+        if size == 256:
+            size = 0
 
-        self.write_bytes(bytes([ord('W'), block, offset, length]))
+        self.write_bytes(bytes([ord('W'), block, offset, size]))
         self.write_bytes(bytes(data))
         self.check_ack()
 
@@ -421,28 +423,30 @@ class TMV71:
         '''Read data from the radio and write it to a file-like object.'''
 
         for block in range(0x7F):
+            addr = block * 256
             LOG.debug('reading block %d', block)
-            data = self.read_block(block, 0, 0)
+            data = self.read_block(addr, 0)
             fd.write(data)
 
     @pm
     def write_memory(self, fd):
         '''Read data from a file-like object and write it to the radio.'''
 
-        data = self.read_block(0, 0, 4)
+        data = self.read_block(0, 4)
         if data != b'\x00\x4b\x01\xff':
             LOG.warning('unexpected content in block 0 (continuing)')
 
-        self.write_block(0, 0, b'\xff')
+        self.write_block(0, b'\xff')
 
         fd.seek(4)
-        self.write_block(0, 4, fd.read(0xfc))
+        self.write_block(0x04, fd.read(0xfc))
 
         for block in range(1, 0x7F):
+            addr = block * 256
             LOG.debug('writing block %d', block)
             data = fd.read(256)
-            self.write_block(block, 0, data)
+            self.write_block(addr, data)
 
-        self.write_block(0, 0, b'\x00\x4b\x01\xff')
+        self.write_block(0, b'\x00\x4b\x01\xff')
 
     # ----------------------------------------------------------------------

tmv71/tests/test_api.py
@@ -81,7 +81,7 @@ class TestApi:
 
     def test_read_block_no_pm(self, radio):
         with pytest.raises(api.WrongModeError):
-            radio.read_block(0, 0, 4)
+            radio.read_block(0, 4)
 
     def test_read_block(self, radio):
         test_data = b'\x01\x02\x03\x04'
@@ -91,7 +91,7 @@ class TestApi:
         radio._port.stuff(b'\x06\x06\r\x00')
 
         with radio.programming_mode():
-            data = radio.read_block(0, 0, 4)
+            data = radio.read_block(0, 4)
             assert radio._port.rx.getvalue().endswith(b'R\x00\x00\x04\x06')
             assert data == test_data
 
@@ -102,8 +102,6 @@ class TestApi:
         assert res['model'] == 'K'
 
     def test_get_port_speed(self, radio):
-        block, offset = api.M_OFFSET_PORT_SPEED
-
         radio._port.stuff(b'0M\rW\x00\x00\x04\x03')
         radio._port.stuff(b'\x06\x06\r\x00')
 
@@ -112,8 +110,6 @@ class TestApi:
             assert res == '57600'
 
     def test_set_port_speed(self, radio):
-        block, offset = api.M_OFFSET_PORT_SPEED
-
         radio._port.stuff(b'0M\r\x06\x06\r\x00')
 
         with radio.programming_mode():

commit 5ea8a281b94c2620e50f969551f4fc7e180a70e6

Fix read-block and write-block cli commands
The changes in 45ae387 broke the read-block and write-block commands.

tmv71/cli.py
@@ -564,18 +564,21 @@ def read_block(ctx, output, _hexdump, address, size):
     This command will by default output the binary data to stdout. Use the
     '-o' option to write to a file instead.
 
-    You can read a range of blocks by specifying the start and end
-    (inclusive) of the range separated by a colon.  E.g., to read blocks 0
-    through 20, you could use `tmv71 memory read-block 0:20`.
+    You can read a range of addresses by specifying the start and end
+    (inclusive) of the range separated by a colon.  E.g., to read addresses
+    0x1700 through 0x557f, you could use `tmv71 memory read-block
+    0x1700:0x557f`.'''
 
-    Nothing stops you from providing an offset and size when specifying
-    multiple blocks, but it probably doesn't make sense.'''
-
-    if ':' in address:
+    if ':' in address and size != 0:
+        raise ValueError('you cannot specify a size '
+                         'when using an address range')
+    elif ':' in address:
         addr_start, addr_end = (flexint(x) for x in address.split(':'))
-        addr_range = range(addr_start, addr_end)
     else:
-        addr_range = [flexint(address)]
+        addr_start = flexint(address)
+        addr_end = addr_start + 256
+
+    addr_range = range(addr_start, addr_end, 256)
 
     with output, ctx.obj.programming_mode():
         for addr in addr_range:
@@ -597,7 +600,7 @@ def read_block(ctx, output, _hexdump, address, size):
 @click.argument('address', type=flexint)
 @click.argument('length', type=flexint, default='0', required=False)
 @click.pass_context
-def write_block(ctx, input, hexdata, block, offset, length):
+def write_block(ctx, input, hexdata, address, length):
     '''Write data to radio memory.
 
     This command will by default read data from stdin. Use the
@@ -617,8 +620,8 @@ def write_block(ctx, input, hexdata, block, offset, length):
     elif datalen == 256:
         datalen = 0
 
-    LOG.info('writing %d bytes of data to block %d offset %d',
-             256 if datalen == 0 else datalen, block, offset)
+    LOG.info('writing %d bytes of data to address %d',
+             256 if datalen == 0 else datalen, address)
 
     with ctx.obj.programming_mode():
-        ctx.obj.write_block(block, offset, data)
+        ctx.obj.write_block(address, data)